98681e87a77245bedfecd11540e81dfb312e272c
Beatriz677 <86676818+Beatriz677@users.noreply.github.com>
2021-07-19 21:37:57 -0500

    Add files via upload

---
+++ Classes.playground/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+class Maya {
+    var age: String
+    var school: String
+    var hobbies: String
+    init (age: String, school: String, hobbies: String) {
+        self.age = age
+        self.school = school
+        self.hobbies = hobbies
+    }
+    
+    var characterBasics = Maya(age: "13", school: "Elite Academy", hobbies: "listens to punk rock and loves human autonomy")
+    
+    }
+
+
